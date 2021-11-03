c477b778924af2d0ad0b4bac214a76fffefc6a92
SHRAY SALVI <btoacpp@gmail.com>
2021-11-03 06:23:41 +0530

    v

--- Arlarse/settings.py
+++ Arlarse/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "templates/"]
-STATIC_ROOT = r'/home/site/wwwroot/templates/'
+# STATICFILES_DIRS = [BASE_DIR / "templates/"]
+STATIC_ROOT = r'/home/site/wwwroot/templates/Events'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = [BASE_DIR / "media/"]
