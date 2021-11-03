f318a481e72d4264edd1c5bc435fcb266eb987e1
SHRAY SALVI <btoacpp@gmail.com>
2021-11-03 08:29:39 +0530

    v

--- Arlarse/settings.py
+++ Arlarse/settings.py
@@ -115,8 +115,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "templates/"]
+STATICFILES_DIRS = [BASE_DIR / "templates/"]
 STATIC_ROOT = BASE_DIR / "Front_End/"
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = [BASE_DIR / "media/"]
