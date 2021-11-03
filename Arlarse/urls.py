7824dc5cb9b31005c6e3574c7a3477b40e539b45
SHRAY SALVI <btoacpp@gmail.com>
2021-11-03 17:52:58 +0530

    v

--- Arlarse/urls.py
+++ Arlarse/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('events/', include("Events.urls")),
+                  path('', include("Events.urls"))
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
