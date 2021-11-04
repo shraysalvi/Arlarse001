e19e4118d9cb51758ba4f08499ec5c663d6635be
SHRAY SALVI <btoacpp@gmail.com>
2021-11-05 04:08:05 +0530

    v

--- Arlarse/settings.py
+++ Arlarse/settings.py
@@ -122,9 +122,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = [BASE_DIR / "media/"]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
