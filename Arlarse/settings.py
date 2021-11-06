5244f25e0d802e51becb8ab188c47192b201b20b
SHRAY SALVI <btoacpp@gmail.com>
2021-11-06 13:55:15 +0530

    v

--- Arlarse/settings.py
+++ Arlarse/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cd)15n0q&o3s3+$5mvo+p1cg)xpov85rovl+1f7a8e%z56m=_%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "templates/"]
+STATICFILES_DIRS = [BASE_DIR / "templates/"]
 STATIC_ROOT = BASE_DIR / "static/"
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
